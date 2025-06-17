2a9593d0cce1bfd8f54d647499e9a74f384b2f9f
wvargas24 <wuilly.vargas22@gmail.com>
2025-06-17 10:45:56 -0400

    Updated conection to DB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const logger = require('../config/logger'); // Importar el logger
 
+// URL de conexión a MongoDB
+const env = process.env.NODE_ENV || 'development'; // Por defecto será 'development'
+const MONGO_URI = env === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_DEV;
+
+// Conexión a MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true, // Estas opciones pueden no ser necesarias en versiones recientes de Mongoose, pero se mantienen por compatibilidad
       useUnifiedTopology: true,
     });
